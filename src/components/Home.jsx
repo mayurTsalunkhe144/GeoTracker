9fa5bb40a38c424fdac5d5814ac0356c655e3abf
mayurTsalunkhe144 <150130741+mayurTsalunkhe144@users.noreply.github.com>
2024-09-11 22:56:25 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
-      <h1 className="font-bold text-blue-600">home</h1>
+      <h1 className="font-bold text-blue-600">Home Page</h1>
     </div>
   );
 }
